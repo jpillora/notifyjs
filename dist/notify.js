575852ad09eb307b20dd1104ced1e2c3248fc8f5
Mark Hetherington <mark@keodesign.com.au>
2022-08-04 10:33:05 +1000

    Update project URL

--- dist/notify.js
+++ dist/notify.js
@@ -1,4 +1,4 @@
-/* Notify.js - http://notifyjs.com/ Copyright (c) 2015 MIT */
+/* Notify.js - https://notifyjs.jpillora.com/ Copyright (c) 2015 MIT */
 (function (factory) {
 	// UMD start
 	// https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
